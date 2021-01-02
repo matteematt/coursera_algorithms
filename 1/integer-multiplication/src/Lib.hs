7e6d1291c8bac8b85d1b0b8b1f446859a4f10ff0
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-02 09:30:02 +0000

    Completed week one programming assignment

--- 1/integer-multiplication/src/Lib.hs
+++ 1/integer-multiplication/src/Lib.hs
@@ -1,10 +1,32 @@
-module Lib (simple) where
+module Lib where
+
+import Data.List
 
 simple :: Integer -> Integer -> Integer
 simple x y = x * y
 
 -- Assignment #1
 -- Implement a multiplication algorithm that operates on single digit numbers as the base case
+x = 3141592653589793238462643383279502884197169399375105820974944592
+y = 2718281828459045235360287471352662497757247093699959574966967627
 
 karatsuba :: Integer -> Integer -> Integer
-karatsuba x y = undefined
+karatsuba x y = if x <= 10 && y <= 10 then x * y else
+                let
+                    largestLen = if (length $ show x) > (length $ show y)
+                                    then (length $ show x) else (length $ show y)
+                    halfn = ceiling $ (fromIntegral largestLen) / 2
+                    a = toInteger $ div x (10 ^ halfn)
+                    b = toInteger $ mod x (10 ^ halfn)
+                    c = toInteger $ div y (10 ^ halfn)
+                    d = toInteger $ mod y (10 ^ halfn)
+                    ac = karatsuba a c
+                    bd = karatsuba b d
+                    expanded = (a+b) * (c+d)
+                    adbc = expanded - ac - bd
+                in 10 ^ (2 * halfn) * ac + 10 ^ halfn * adbc + bd
+
+-- Unused as can use (length $ show x) which appears as fast and less space required
+base10digits :: Integer -> Integer
+base10digits x = go x 1
+    where go x i = if x > (10^i) then go x (i+1) else i
--- 1/integer-multiplication/test/Spec.hs
+++ 1/integer-multiplication/test/Spec.hs
@@ -1,13 +1,20 @@
 import Test.QuickCheck
 import Lib
 
+val1 = 3141592653589793238462643383279502884197169399375105820974944592
+val2 = 2718281828459045235360287471352662497757247093699959574966967627
+
 assert :: Bool -> String -> String -> String
 assert x pass fail = if x then pass else fail
 
 prop_simpleMulti x y = simple x y == x * y
 
+-- Does not work as numbers are not the same length
+prop_karatsubaMulti x y = karatsuba x y == x * y
+
 main :: IO ()
 main = do
   putStrLn $ assert (3 == 3) "3 is equal to 3" "3 is not equal to 3"
   quickCheck prop_simpleMulti
+  quickCheck prop_karatsubaMulti
   putStrLn "Done with tests!"
