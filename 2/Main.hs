fa3dace2fca2e69d49f4fbdeeb142073e50fe9eb
Matt Walker <matthew.walker@sky.uk>
2021-01-08 15:17:46 +0000

    Completed merge sort

--- 2/Main.hs
+++ 2/Main.hs
@@ -1,8 +1,23 @@
 module Main where
 
+t1 = [3, 5, 20, 53] :: [Int]
+t2 = [11, 15, 25, 40] :: [Int]
 
-mergeSort :: [Int] -> [Int] -> [Int]
-mergeSort x y
+divideAndConquer :: Ord a => ([a] -> [a] -> [a]) -> [a] -> [a]
+divideAndConquer _ [] = []
+divideAndConquer _ [x] = [x]
+divideAndConquer _ [x1,x2] = if x1 <= x2 then [x1,x2] else [x2,x1]
+divideAndConquer fn xs = let (lhs,rhs) = splitAt (div (length xs) 2) xs
+                             lhs' = divideAndConquer mergeSort lhs
+                             rhs' = divideAndConquer mergeSort rhs
+                          in fn lhs' rhs'
+
+mergeSort :: Ord a => [a] -> [a] -> [a]
+mergeSort [] [] = []
+mergeSort (x:xs) [] = x : mergeSort xs []
+mergeSort [] (y:ys) = y : mergeSort [] ys
+mergeSort x@(x1:xs) y@(y1:ys) = if x1 <= y1 then x1 : mergeSort xs y
+                                            else y1 : mergeSort x ys
 
 main :: IO ()
 main = undefined
