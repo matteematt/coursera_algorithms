2bd431c7bbf840531d65d800342898492f1da008
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-14 21:27:57 +0000

    Added basic quicksort implementation in scala

--- 3/src/main/scala/main.scala
+++ 3/src/main/scala/main.scala
@@ -1,14 +1,69 @@
 package quicksort
 
 import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
 
 object Main {
 
   val filePath = "/home/matt/documents/coursera_algorithms/3/src/main/resources/input.txt"
+  val input: List[Int] = Source.fromFile(filePath).getLines.toList.map(_.toInt)
+
+  def takeFirst(x: ArrayBuffer[Int]): Int = 0
+
+  def swapIndices(x: ArrayBuffer[Int], l: Int, r: Int): Unit = {
+    val tmp = x(r)
+    x(r) = x(l)
+    x(l) = tmp
+  }
+
+  def quickSort(x: List[Int], pivotChoice: ArrayBuffer[Int] => Int): List[Int] = {
+    def go(x: ArrayBuffer[Int], l: Int, r: Int): Unit = {
+      if (Math.abs(l-r) > 1) {
+        // Calculate the pivot index using the pivotChoice function
+        val subArray = x.drop(l).take(r)
+        val pivotIndex = pivotChoice(subArray) + l
+
+        // Ensure that the pivot is moved to the first position
+        // at position x(l)
+        if (pivotIndex != l) swapIndices(x, pivotIndex, l)
+
+        var i = l+1
+        var j = l+1
+
+        // Iterate over the unpartitioned array
+        for (j <- i to r) {
+          // If the current value is less than the pivot
+          if (x(j) < x(l)) {
+            // Swap it with i, which points to the right-most number less than the pivot
+            swapIndices(x, j, i)
+            // Increment the i to count the <p as getting one larger
+            i+=1
+          }
+        }
+        // Move the pivot into the correct position
+        swapIndices(x, l, (i-1))
+
+        go(x, l, (i-1))
+        go(x, i, r)
+      }
+      // Hit base case so stop recursion
+    }
+
+    val xarr = x.to(ArrayBuffer)
+    go(xarr, 0, (xarr.size-1))
+    xarr.toList
+  }
 
   def main(args: Array[String]): Unit = {
     println("Hello World")
-    val input = Source.fromFile(filePath).getLines
-    println(input.max)
+    var arr = input.to(ArrayBuffer)
+    println(arr.min)
+
+    println(arr.take(5))
+    swapIndices(arr, 1, 3)
+    println(arr.take(5))
+
+    println(quickSort(input.toList, takeFirst).take(25))
   }
+
 }
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Workings for the coursera algorithms course taken 2021
 
 Each directory contains the working out for each week on the course
+
+1. Haskell Karatsuba method multiplication
+2. Haskell Merge Sort
+3. Scala QuickSort (not using haskell as proper haskell implementation is currently outside of my
+	 skill-set)
