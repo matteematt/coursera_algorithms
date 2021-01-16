ed7689223eccace502a25b13c7764aa405bb491d
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-16 09:56:02 +0000

    Completed programming problem 3

--- 3/src/main/scala/main.scala
+++ 3/src/main/scala/main.scala
@@ -10,17 +10,32 @@ object Main {
 
   def takeFirst(x: ArrayBuffer[Int]): Int = 0
 
+  def takeLast(x: ArrayBuffer[Int]): Int = x.length - 1
+
+  def medianOfThree(x: ArrayBuffer[Int]): Int = {
+    var middleIndex = if (x.length % 2 == 0) ((x.length/2)-1) else (x.length/2)
+    var lastIndex = x.length - 1
+    var choicePairs = Array((0, x(0)), (middleIndex, x(middleIndex)), (lastIndex, x(lastIndex)))
+    val median = choicePairs.map(_._2).sortWith(_ < _).drop(choicePairs.length/2).head
+    choicePairs.filter({ case (index,value) => value == median}).head._1
+  }
+
   def swapIndices(x: ArrayBuffer[Int], l: Int, r: Int): Unit = {
     val tmp = x(r)
     x(r) = x(l)
     x(l) = tmp
   }
 
-  def quickSort(x: List[Int], pivotChoice: ArrayBuffer[Int] => Int): List[Int] = {
+  def quickSort(x: List[Int], pivotChoice: ArrayBuffer[Int] => Int): (Int,List[Int]) = {
+    var comparisons = 0
+
     def go(x: ArrayBuffer[Int], l: Int, r: Int): Unit = {
       if (Math.abs(l-r) > 1) {
+        // Increment the number of comparisons made
+        comparisons += Math.abs(l-r) - 1
+
         // Calculate the pivot index using the pivotChoice function
-        val subArray = x.drop(l).take(r)
+        val subArray = x.drop(l).take(r-l)
         val pivotIndex = pivotChoice(subArray) + l
 
         // Ensure that the pivot is moved to the first position
@@ -31,7 +46,7 @@ object Main {
         var j = l+1
 
         // Iterate over the unpartitioned array
-        for (j <- i to r) {
+        for (j <- i to (r-1)) {
           // If the current value is less than the pivot
           if (x(j) < x(l)) {
             // Swap it with i, which points to the right-most number less than the pivot
@@ -50,20 +65,26 @@ object Main {
     }
 
     val xarr = x.to(ArrayBuffer)
-    go(xarr, 0, (xarr.size-1))
-    xarr.toList
+    go(xarr, 0, (xarr.size))
+    (comparisons, xarr.toList)
   }
 
   def main(args: Array[String]): Unit = {
-    println("Hello World")
-    var arr = input.to(ArrayBuffer)
-    println(arr.min)
+    var (firstCount, firstSorted) = quickSort(input.toList, takeFirst)
+    println("=====Take First=====")
+    println(firstSorted.take(5))
+    println(firstCount)
 
-    println(arr.take(5))
-    swapIndices(arr, 1, 3)
-    println(arr.take(5))
+    var (lastCount, lastSorted) = quickSort(input.toList, takeLast)
+    println("=====Take last=====")
+    println(lastSorted.take(5))
+    println(lastCount)
 
-    println(quickSort(input.toList, takeFirst).take(25))
+    var (motCount, motSorted) = quickSort(input.toList, medianOfThree)
+    println("=====Take mot=====")
+    println(motSorted.take(5))
+    println(motCount)
+
   }
 
 }
