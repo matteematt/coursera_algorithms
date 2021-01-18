52566c20869dc66ab9c61169366d781d1eb1cdc6
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-18 21:19:32 +0000

    Finished programming assignment 4

--- 4/src/main/scala/Main.scala
+++ 4/src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-// package karger
+package karger
 
 import scala.io.Source
 
@@ -6,34 +6,21 @@ case class NodeItem(var n: String, var es: List[String])
 
 object Main {
 
-  val inputLines = Source.fromFile(path("test")).getLines().toList
-
   def path(x: String) = s"/home/matt/documents/coursera_algorithms/4/src/main/resources/$x.txt"
 
   def getNodeList(x: String) =
     Source.fromFile(path(x))
       .getLines().toList
-      .map(_.split(" ").toList
+      .map(_.split("\\s+").toList
         match { case (h :: t) => NodeItem(h,t)})
 
   def chooseNodes(nodeList: List[NodeItem]) = {
     var r = scala.util.Random
-    var intersectPairs: List[(NodeItem,NodeItem)] = List()
-
-    while (intersectPairs.length == 0) {
-      val shuffledNodes = r.shuffle(nodeList)
-      val pairedNodes = shuffledNodes.zip(shuffledNodes.tail)
-      intersectPairs = pairedNodes.filter {
-        case (ni1,ni2) =>
-          ni1.es.contains(ni2.n) && ni2.es.contains(ni1.n)
-      }
-      // println(intersectPairs)
-      // System.exit(0)
-    }
-
-    println(intersectPairs)
-    val (ni1,ni2) = intersectPairs.head
-    (ni1.n,ni2.n)
+    // Randomly pick a node
+    val ni1 = r.shuffle(nodeList).head
+    // Randomly pick an edge from that node
+    val edge = r.shuffle(ni1.es).head
+    (ni1.n,edge)
   }
 
   def mergeNodePair(nodeList: List[NodeItem]) = {
@@ -41,7 +28,7 @@ object Main {
     val mergedName = ni1.n + "|" + ni2.n
     val mergedEdgeList = ni1.es.appendedAll(ni2.es).map {
       x => if (x == ni1.n || x == ni2.n) mergedName else x
-    }.distinct.filter { _ != mergedName }
+    }.filter { _ != mergedName }
     NodeItem(mergedName, mergedEdgeList)
   }
 
@@ -55,25 +42,29 @@ object Main {
       while (nodeList.length > 2) {
         // Choose an edge to reduce
         val chosenNodes = chooseNodes(nodeList)
-        println(chosenNodes)
+        // Partition out the two chosen nodes
         val (chosen,rest) = nodeList.partition
           { case NodeItem(n,_) => n == chosenNodes._1 || n == chosenNodes._2 }
+        // Merge the two selected nodes
         val merged = mergeNodePair(chosen)
+        // Update the rest of the nodes with the merged nodes info
         val updatedRest = rest.map {
           case NodeItem(n,es) =>
             val updatedEdges = es.map
               { x => if (x == chosenNodes._1 || x == chosenNodes._2) merged.n else x }
             NodeItem(n, updatedEdges)
         }
+        // Place the merged node into the node list
         nodeList = updatedRest.appendedAll(List(merged))
-        println(nodeList)
       }
-      nodeList.flatMap(_.es).length
+      // The min cut of this iteration is the number of edges from one node to another
+      nodeList.head.es.length
     }
 
+    // Run n times, keeping the lowest score
     var lowest = 100000000
     for (i <- 0 to (nl.length)) {
-      println(s"Run $i of ${nl.length}")
+      // println(s"Run $i of ${nl.length}")
       val minCut = go()
       lowest = if (minCut < lowest) minCut else lowest
     }
@@ -82,10 +73,8 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     println("Karger Min Cuts Algorithm")
-    println(inputLines.mkString("\n"))
-
     println("\nRun:\n")
-    // val minCut = karger(getNodeList("test"))
+    // println(karger(getNodeList("test")))
     println(karger(getNodeList("input")))
   }
 }
