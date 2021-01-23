12887116783a34cb8506cfe9eb9f7ba372ba3dfe
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-23 15:35:29 +0000

    Parsed input ready for algorithm

--- 5/Main.hs
+++ 5/Main.hs
@@ -1,20 +1,37 @@
 module Main where
 
+import Data.List
 import qualified Data.Map as M
 
 main :: IO ()
 main = undefined
 
-adjacencyList = map (map (read :: String -> Int) . words) $ lines $ testData
-nodeList = M.fromList $ zip [1..((length adjacencyList) -1)] (repeat ([],[]))
+testGraph = buildGraph testData
+
+
+-- Build the graph from input
 
 addEdge :: M.Map Int ([Int],[Int]) -> (Int,Int) -> M.Map Int ([Int],[Int])
-addEdge m (n,e) = undefined
+addEdge m (n,e) = let m' = mapValTransform m n (pushLHS e)
+                   in mapValTransform m' e (pushRHS n)
 
-mapUpdate :: Ord k => M.Map k a -> k -> (a -> a) -> M.Map k a
-mapUpdate m k fn = let curr = M.lookup k m
-                    in case curr of Nothing -> m
-                                    Just (v) -> M.insert k (fn v) m
+mapValTransform :: Ord k => M.Map k a -> k -> (a -> a) -> M.Map k a
+mapValTransform m k fn = let curr = M.lookup k m
+                          in case curr of Nothing -> m
+                                          Just (v) -> M.insert k (fn v) m
+
+pushLHS :: Int -> ([Int],[Int]) -> ([Int],[Int])
+pushLHS x (l,r) = (x:l,r)
+
+pushRHS :: Int -> ([Int],[Int]) -> ([Int],[Int])
+pushRHS x (l,r) = (l,x:r)
+
+
+buildGraph x = let adjacencyList =
+                     map ((\(a:b:_) -> (a,b)) . map (read :: String -> Int) . words) $ lines $ x
+                   nodesCount = length $ group $ sort $ map (\(x,_) -> x) adjacencyList
+                   nodeList = M.fromList $ zip [1..nodesCount] (repeat ([],[]))
+                in foldr (\nodeEdge m -> addEdge m nodeEdge) nodeList adjacencyList
 
 testData = "1 3\n\
   \2 1\n\
