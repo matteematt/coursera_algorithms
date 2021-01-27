22e25915408ba56a5958f907053f8fadeee9f189
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-27 19:31:11 +0000

    Can add elements to heap

--- 6/src/main/scala/heap.scala
+++ 6/src/main/scala/heap.scala
@@ -1,6 +1,7 @@
 package dijkstra
 
 import scala.collection.mutable.ArrayBuffer
+import scala.annotation.tailrec
 
 /*
  * Need to implement
@@ -11,15 +12,46 @@ import scala.collection.mutable.ArrayBuffer
 class Heap {
   val arr = ArrayBuffer[Int]()
 
+  // Public Methods
   def isEmpty(): Boolean = arr.length == 0
 
-  def peekMin(): Option[Int] = None
+  def peekMin(): Option[Int] = {
+    if (arr.length == 0) None
+    else {
+      Some(arr(0))
+    }
+  }
 
-  def popMin(): Option[Int] = None
+  def popMin(): Option[Int] = {
+    if (arr.length == 0) None
+    else {
+      val min = arr(0)
+      arr -= min
+      Some(min)
+    }
+  }
 
   def add(x: Int): Unit = {
-    arr(0) = x
+    arr.append(x)
+    bubbleUp(arr.length-1)
   }
 
   override def toString(): String = arr.toString
+
+  // Private Methods
+  private def swap(li: Int, ri: Int): Unit = {
+    val tmp = arr(li)
+    arr(li) = arr(ri)
+    arr(ri) = tmp
+  }
+
+  @tailrec
+  private def bubbleUp(index: Int): Unit = {
+    val parentIndex = ((index-1)/2).floor.toInt
+    if (arr(parentIndex) <= arr(index)) return
+    else {
+      swap(parentIndex, index)
+      bubbleUp(parentIndex)
+    }
+  }
 }
--- 6/src/test/scala/heap.scala
+++ 6/src/test/scala/heap.scala
@@ -23,10 +23,55 @@ class FirstSpec extends AnyWordSpec  {
         assert(heap.popMin() == None)
       }
 
-      "be non-empty after inserting a value" in {
-        // val heap = new Heap()
-        // heap.add(5)
-        // assert(heap.isEmpty() == false)
+    }
+
+    "with a value" should {
+
+      "be non-empty" in {
+        val heap = new Heap()
+        heap.add(5)
+        assert(heap.isEmpty() == false)
+      }
+
+      "show the minimum value when using peekMin" in {
+        val heap = new Heap()
+        heap.add(5)
+        assert(heap.peekMin() == Some(5))
+        assert(heap.isEmpty() == false)
+      }
+
+      "return the minimum value when using popMin, decreasing heap size" in {
+        val heap = new Heap()
+        heap.add(5)
+        assert(heap.popMin() == Some(5))
+        assert(heap.isEmpty() == true)
+      }
+
+      "return the minimum value when using popMin, after adding a new smaller value" in {
+        val heap = new Heap()
+        heap.add(5)
+        heap.add(3)
+        assert(heap.popMin() == Some(3))
+        assert(heap.isEmpty() == false)
+      }
+    }
+
+    "filled with random numbers" should {
+      "return the numbers in increasing order" in {
+        val heap = new Heap()
+
+        val r = scala.util.Random
+        val randomVals = Iterator.unfold(0){
+          case x if x < 40 => Some((r.nextInt(200), x+1))
+          case _ => None
+        }
+        // randomVals.fold(heap){
+          // (x: Heap,h: Int) => h.add(x)
+        // }
+        randomVals.fold(heap) {
+          (h,i) => h.add(i)
+        }
+        println(randomVals.mkString(","))
       }
     }
   }
