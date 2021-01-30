329aeb6e37ac6686a0e63ee5383117b54e5d2be5
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-30 10:07:03 +0000

    Finished main loop but not working

--- 6/src/main/scala/main.scala
+++ 6/src/main/scala/main.scala
@@ -2,6 +2,7 @@ package dijkstra
 
 import scala.io.Source
 import scala.collection.mutable.Map
+import scala.annotation.tailrec
 
 case class Node(n: Int, paths: List[Tuple2[Int,Int]], var score: Option[Int])
 
@@ -41,11 +42,44 @@ object Graph {
   }
 }
 
-object Seach {
+object Search {
   val unconnected = 1000000
 
   def dijkstra(graph: Map[Int,Node], from: Int, to: Int): Int = {
-    ???
+    val heap = new Heap[Path]()
+    // Initial score to this path is 0
+    go(graph,heap,0,from,to)
+  }
+
+  @tailrec
+  private def go(graph: Map[Int,Node], heap: Heap[Path], pathScore: Int, from: Int, to: Int): Int = {
+    // Get the new paths at the frontier
+    val node = graph.get(from).getOrElse(throw new Error(s"Unable to find node $from in graph"))
+    // Set the nodes score TODO: Needs the path length adding
+    node.score = Some(pathScore)
+
+    // Get all the new paths from the increased frontier
+    val paths = node.paths.map { case (head,length) =>
+      Path(from,head,(node.score.get + length))
+    }
+
+    // Update the heap with all thew new paths, but only if the new one is a smaller size
+    // TODO: could be made more efficient by not ripping the old one out if its better (smaller)
+    paths.foreach { x =>
+      val oldVal = heap.deleteArbitrary(y => x.head == y.head)
+      if (oldVal.isDefined && oldVal.get.score < x.score) heap.add(oldVal.get)
+      else heap.add(x)
+    }
+    // println(heap)
+
+    // If bestPath = None then no path and we need to return unconnected
+    // TODO: Check the below code works (might need to throw and catch)
+    val bestPath = heap.popMin().getOrElse(throw new Error(s"Unable to find a next path"))
+    val scoreToHead = node.score.get + bestPath.score
+    val newNode = bestPath.head
+
+    if (bestPath.head == to) scoreToHead
+    else go(graph, heap, scoreToHead, newNode, to)
   }
 }
 
@@ -54,32 +88,9 @@ object Main {
     println("\n\nDijkstra's")
 
     val graph = Graph.parseGraphFromFile("input")
-    // println(graph)
-
-    // Example dijkstra
-    val heap = new Heap[Path]()
-    val from = 1
-    val to = 7
-    val score = 0
-
-
-    // Get the new paths at the frontier
-    val node = graph.get(from).getOrElse(throw new Error(s"Unable to find node $from in graph"))
-    // Set the nodes score TODO: Needs the path length adding
-    node.score = Some(score)
-    // println(node)
-    // Get all the new paths from the increased frontier
-    val paths = node.paths.map { case (head,length) =>
-      Path(from,head,(node.score.get + length))
-    }
-    // println(paths)
-    // Update the heap with all thew new paths, but only if the new one is a smaller size
-    // TODO: could be made more efficient by not ripping the old one out if its better (smaller)
-    paths.foreach { x =>
-      val oldVal = heap.deleteArbitrary(y => x.head == y.head)
-      if (oldVal.isDefined && oldVal.get.score < x.score) heap.add(oldVal.get)
-      else heap.add(x)
-    }
-    println(heap)
+    val toList = List(7,37,59,82,99,115,133,165,188,197)
+    val outList = toList.map(x => Search.dijkstra(graph, 1, 7))
+    println(outList)
+    // println(Search.dijkstra(graph, 1, 7))
   }
 }
