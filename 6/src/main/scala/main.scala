962fbb52b45004ed48d763ea2744bf69b058ba9c
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-30 13:49:48 +0000

    Fixed algorithm not looking accounting for visited nodes

--- 6/src/main/scala/main.scala
+++ 6/src/main/scala/main.scala
@@ -48,7 +48,6 @@ object Search {
   def dijkstra(graph: Map[Int,Node], from: Int, to: Int): Int = {
     val heap = new Heap[Path]()
     val visited = Map[Int,Boolean]()
-    // Initial score to this path is 0
     go(graph,heap,visited,0,from,to)
   }
 
@@ -60,7 +59,6 @@ object Search {
     pathScore: Int,
     from: Int,
     to: Int): Int = {
-    println(s"\nAt $from")
     // Get the new paths at the frontier
     val node = graph.get(from).getOrElse(throw new Error(s"Unable to find node $from in graph"))
     visited.addOne(node.n,true)
@@ -71,7 +69,6 @@ object Search {
     val paths = node.paths.map { case (head,length) =>
       Path(from,head,(graph.get(from).get.score.get + length))
     }.filter(x => !visited.contains(x.head))
-    println(s"Path set : ${paths.mkString(",")}")
 
     // Update the heap with all thew new paths, but only if the new one is a smaller size
     // TODO: could be made more efficient by not ripping the old one out if its better (smaller)
@@ -80,37 +77,21 @@ object Search {
       if (oldVal.isDefined && oldVal.get.score < x.score) heap.add(oldVal.get)
       else heap.add(x)
     }
-    println(s"Heap : $heap")
-    // println(heap)
-
     // If bestPath = None then no path and we need to return unconnected
-    // TODO: Check the below code works (might need to throw and catch)
+    // TODO: Need to implement the throw and catch properly for unconnected graphs
     val bestPath = heap.popMin().getOrElse(throw new Error(s"Unable to find a next path"))
-    // val scoreToHead = bestPath.score + graph.get(bestPath.tail).get.score.get
-    val scoreToHead = bestPath.score
-    val newNode = bestPath.head
 
-    println(s"tail: ${graph.get(bestPath.tail).get}")
-    println(s"bestPath.score: ${bestPath.score}")
-    println(s"bestPath: $bestPath, scoreToHead: $scoreToHead, newNode: $newNode")
-
-    if (bestPath.head == to) scoreToHead
-    else go(graph, heap, visited, scoreToHead, newNode, to)
+    if (bestPath.head == to) bestPath.score
+    else go(graph, heap, visited, bestPath.score, bestPath.head, to)
   }
 }
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("\n\nDijkstra's")
-
-    // val graph = Graph.parseGraphFromFile("test")
-    // println(graph)
-    // println(Search.dijkstra(graph,1,6))
 
     val graph = Graph.parseGraphFromFile("input")
     val toList = List(7,37,59,82,99,115,133,165,188,197)
-    val outList = toList.map(x => Search.dijkstra(graph, 1, 7))
-    println(outList)
-    // println(Search.dijkstra(graph, 1, 7))
+    val outList = toList.map(x => Search.dijkstra(graph, 1, x))
+    println(outList.mkString(","))
   }
 }
