a283acb00a45414b1dc7db39b1bcd6279820ffdd
Matt Arch Linux <mattwalker340@gmail.com>
2021-01-30 09:32:22 +0000

    Created first iteration of dijkstra

--- 6/src/main/scala/main.scala
+++ 6/src/main/scala/main.scala
@@ -1,35 +1,85 @@
 package dijkstra
 
 import scala.io.Source
+import scala.collection.mutable.Map
 
-case class Node(n: Int, paths: List[Tuple2[Int,Int]])
+case class Node(n: Int, paths: List[Tuple2[Int,Int]], var score: Option[Int])
 
-object Main {
-  def filePath(x: String) = s"/home/matt/documents/coursera_algorithms/6/${x}.txt"
+// Where score is dijkstra's greedy score
+case class Path(tail: Int, head: Int, score: Int) extends Ordered[Path] {
+  def compare(that: Path) = this.score - that.score
+}
 
-  def readInput(file: String): List[String] =
+object Graph {
+  private def filePath(x: String) = s"/home/matt/documents/coursera_algorithms/6/${x}.txt"
+
+  private def readInput(file: String): List[String] =
     Source.fromFile(filePath(file))
     .getLines().toList
 
-  def parseTuple(x: String): Tuple2[Int,Int] = x.split(",") match {
+  private def parseTuple(x: String): Tuple2[Int,Int] = x.split(",") match {
     case Array(a,b,_*) => (a.toInt,b.toInt)
     case _ => throw new Error(s"Unable to parseTuple '${x}'")
   }
 
 
-  def parseGraph(xs: List[String]): List[Node] = {
+  private def parseGraph(xs: List[String]): List[Node] = {
     xs.map(_.split("\\s+").toList match {
-      case h :: t => Node(h.toInt, t.map(x => parseTuple(x)))
+      case h :: t => Node(h.toInt, t.map(x => parseTuple(x)), None)
       case _ => throw new Error(s"Unable to parse input line")
     })
   }
 
+  // Public methods
+  def parseGraphFromFile(file: String): Map[Int,Node] = {
+    val listGraph = parseGraph(readInput(file))
+    val graph = Map[Int,Node]()
+    for (node <- listGraph) {
+      graph.addOne(node.n, node)
+    }
+    graph
+  }
+}
+
+object Seach {
+  val unconnected = 1000000
+
+  def dijkstra(graph: Map[Int,Node], from: Int, to: Int): Int = {
+    ???
+  }
+}
+
+object Main {
   def main(args: Array[String]): Unit = {
     println("\n\nDijkstra's")
 
-    val graph = parseGraph(readInput("input"))
-    println(graph)
+    val graph = Graph.parseGraphFromFile("input")
+    // println(graph)
 
-    val heap = new Heap[Int]()
+    // Example dijkstra
+    val heap = new Heap[Path]()
+    val from = 1
+    val to = 7
+    val score = 0
+
+
+    // Get the new paths at the frontier
+    val node = graph.get(from).getOrElse(throw new Error(s"Unable to find node $from in graph"))
+    // Set the nodes score TODO: Needs the path length adding
+    node.score = Some(score)
+    // println(node)
+    // Get all the new paths from the increased frontier
+    val paths = node.paths.map { case (head,length) =>
+      Path(from,head,(node.score.get + length))
+    }
+    // println(paths)
+    // Update the heap with all thew new paths, but only if the new one is a smaller size
+    // TODO: could be made more efficient by not ripping the old one out if its better (smaller)
+    paths.foreach { x =>
+      val oldVal = heap.deleteArbitrary(y => x.head == y.head)
+      if (oldVal.isDefined && oldVal.get.score < x.score) heap.add(oldVal.get)
+      else heap.add(x)
+    }
+    println(heap)
   }
 }
