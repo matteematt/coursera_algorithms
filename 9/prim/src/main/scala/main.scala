d16ce97cb32e3c5e52e72d87d7aa99176398102a
Matt Arch Linux <mattwalker340@gmail.com>
2021-02-14 11:00:10 +0000

    PA 9 Q 3 working for test cases but not main input

--- 9/prim/src/main/scala/main.scala
+++ 9/prim/src/main/scala/main.scala
@@ -2,8 +2,14 @@ package coursera
 
 import scala.io.Source
 import scala.collection.mutable.Map
+import scala.collection.mutable.Set
+import scala.annotation.tailrec
 
 case class Node(name: String, edges: List[Tuple2[String,Int]])
+
+case class Edge(from: String, to: String, weight: Int) extends Ordered[Edge] {
+  def compare(that: Edge) = this.weight - that.weight
+}
 
 object File {
   def inputFile(fileName: String) =
@@ -45,12 +51,62 @@ object File {
   }
 }
 
+object Prim {
+  def mst(graph: Map[String,Node]): Int = {
+    // Randomly choose one vertex
+    // Add vertex length to count, add next vertices to heap
+    // choose the shortest node to a node we haven't seen
+    // keep going until we have seen all
+
+    // Init
+    var seen = Set[String]()
+    var heap = new Heap[Edge]()
+
+    val r = scala.util.Random
+    val nodes = graph.keySet.toVector
+    val fst = nodes(r.nextInt(nodes.size))
+
+    go(fst,seen,heap,graph,0)
+  }
+
+  @tailrec
+  private def go(
+    next: String,
+    seen: Set[String],
+    heap: Heap[Edge],
+    graph: Map[String,Node],
+    mstLen: Int
+  ): Int = {
+    // 1st Iter
+    val node = graph.get(next).get
+    seen.addOne(next)
+    println(seen)
+
+    node.edges.filter(x => !seen.contains(x._1)).map {
+      case (to: String, weight: Int) => Edge(next,to,weight)
+    }.foreach(x => heap.add(x))
+
+    heap.cullBy(edge => seen.contains(edge.to))
+
+    // println(heap)
+
+
+    if (heap.isEmpty) mstLen
+    else {
+      val chosenEdge = heap.popMin().get
+      go(chosenEdge.to,seen,heap,graph,mstLen + chosenEdge.weight)
+    }
+  }
+}
+
 object Main {
+  // ans not -3424815
 
   def main(args: Array[String]): Unit = {
     println("\n\nPrim's")
-    val graph = File.parseFileToGraph("tc1")
+    val graph = File.parseFileToGraph("input")
     println(graph)
+    println(Prim.mst(graph))
   }
 
 }
