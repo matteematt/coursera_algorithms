6f6dfba31c3d46e97bbddcf71b037fd9a51a9817
Matt Arch Linux <mattwalker340@gmail.com>
2021-02-16 20:07:14 +0000

    Added basic implementation of unionfind

--- 10/src/main/scala/unionfind.scala
+++ 10/src/main/scala/unionfind.scala
@@ -1,5 +1,51 @@
 package coursera
 
-class UnionFind() {
+import scala.collection.mutable.Map
 
+private case class UFVal(x: Int, var leader: Option[UFVal])
+
+/*
+ * Need to implement:
+ * makeSet
+ * find
+ * merge
+ * setCount
+ * Make for int first and then generic
+ */
+class UnionFind() {
+  private var setC = 0
+  private val collection = Map[Int,UFVal]()
+
+  // Public methods
+  def setCount() = setC
+
+  def find(x: Int): Option[Int] = findUFVal(x).map(_.x)
+
+  def merge(x: Int, y: Int): Unit = {
+    val xLead = findUFVal(x)
+    val yLead = findUFVal(y)
+    if (xLead != None && yLead != None && yLead != xLead) {
+      xLead.get.leader = Some(yLead.get)
+      setC -= 1
+    }
+  }
+
+  def makeSet(x: Int): Unit = {
+    if (collection.contains(x)) return
+    else {
+      setC += 1
+      collection.addOne((x, UFVal(x, None)))
+    }
+  }
+
+  // Private methods
+  private def findUFVal(x: Int): Option[UFVal] = {
+    collection.get(x).map { ufval =>
+      var parent = ufval
+      while (parent.leader != None) {
+        parent = parent.leader.get
+      }
+      parent
+    }
+  }
 }
--- 10/src/test/scala/unionfind.scala
+++ 10/src/test/scala/unionfind.scala
@@ -8,12 +8,86 @@ class UnionFindSpec extends AnyWordSpec {
 
     "initialised" should {
 
-      "be empty" in {
-
+      "have 0 sets" in {
+        val uf = new UnionFind()
+        assert(uf.setCount == 0)
       }
 
+      "return None when trying to find a value" in {
+        val uf = new UnionFind()
+        assert(uf.find(23) == None)
+      }
+
+      "do nothing when trying to merge nonexistent sets" in {
+        val uf = new UnionFind()
+        uf.merge(12,24)
+        assert(true)
+      }
+
+      "be able to retrieve the leader and have 1 set after adding a value" in {
+        val uf = new UnionFind()
+        uf.makeSet(12)
+        assert(uf.setCount == 1)
+        assert(uf.find(12) == Some(12))
+      }
     }
 
+    "filled with two singletons" should {
+
+      "have 2 sets" in {
+        val uf = new UnionFind()
+        uf.makeSet(12)
+        uf.makeSet(13)
+        assert(uf.setCount == 2)
+      }
+
+      "have 1 set after merging the two values" in {
+        val uf = new UnionFind()
+        uf.makeSet(12)
+        uf.makeSet(13)
+        uf.merge(12,13)
+        assert(uf.setCount == 1)
+      }
+
+      "have 1 set after trying to merge the values again" in {
+        val uf = new UnionFind()
+        uf.makeSet(12)
+        uf.makeSet(13)
+        uf.merge(12,13)
+        uf.merge(12,13)
+        assert(uf.setCount == 1)
+      }
+    }
+
+    "filled with two merged values" should {
+
+      "have the same parent when finding" in {
+        val uf = new UnionFind()
+        uf.makeSet(12)
+        uf.makeSet(13)
+        uf.merge(12,13)
+        assert(uf.find(12) == uf.find(13))
+      }
+    }
+
+    "filled with multiple singletons" should {
+
+      "decrease the sets after every new merge, and then the values should have the same leader" in {
+        val uf = new UnionFind()
+        0 to 6 foreach (x => uf.makeSet(x))
+
+        var setCount = 7
+        assert(uf.setCount == setCount)
+
+        1 to 6 foreach { x =>
+          assert(uf.find(0) != uf.find(x))
+          uf.merge(0,x)
+          assert(uf.find(0) == uf.find(x))
+          assert(uf.setCount == (setCount - 1))
+          setCount -= 1
+        }
+      }
+    }
   }
 
 }
